5095ff413552ec7a843074c677437b803365eccc
everything-everything <85025743+everything-everything@users.noreply.github.com>
2021-06-14 11:39:59 +0200

    Delete bin directory

--- bin/Status.sh
+++
@@ -1,6 +0,0 @@
-script -a ./Ergebnis/Status.txt -c "./bin/sem-6000.exp Volt --status --print"
-cat ./Ergebnis/Status.txt | tr -d "\r" | tr . , >./Ergebnis/Status1.txt
-cp ./Ergebnis/Status1.txt ./Ergebnis/Status.txt
-rm ./Ergebnis/Status1.txt
-exit
-
