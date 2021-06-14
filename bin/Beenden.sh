fa21f58c63aaeafc9816e2e008c191398dda2eeb
everything-everything <85025743+everything-everything@users.noreply.github.com>
2021-06-14 15:58:52 +0200

    Add files via upload

---
+++ bin/Beenden.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+# A menu driven shell script sample template 
+## ----------------------------------
+# Step #1: Define variables
+# ----------------------------------
+EDITOR=vim
+RED='\033[0;41;30m'
+STD='\033[0;0;39m'
+ 
+# ----------------------------------
+# Step #2: User defined function
+# ----------------------------------
+pause(){
+  read -p "Drücken Sie [Enter] um Fortzufahren ..." fackEnterKey
+}
+
+show_menus(){
+	clear
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-"	
+	echo "|									 |"
+	echo "| 			     Voltcraft SEM6000 				 |"
+	echo "| 				Beenden 				 |"
+	echo "| 									 |"	
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-"
+	echo "	    "
+	echo "	    1. Zurück zum Hauptmenü                              "
+	echo "	    2. Linux-Konsole    "
+	echo "	    3. Beenden"
+	echo "	    				  "
+	echo "	    "
+}
+
+
+one(){
+	echo "Hauptmenü"
+	./Run.sh
+	break 0
+	}
+
+
+
+
+
+
+two(){
+	echo "Linux-Konsole"
+	pause
+	/bin/bash
+	exit 
+	
+	}     
+
+
+
+three(){
+	echo "Beenden"
+	sleep 1
+	killall konsole
+	killall bash
+	exit 0
+	}
+
+
+
+# read input from the keyboard and take a action
+# invoke the one() when the user select 1 from the menu option.
+# invoke the two() when the user select 2 from the menu option.
+# Exit when user the user select 3 form the menu option.
+
+
+
+
+read_options(){
+	local choice
+	read -p "Enter choice [ 1 - 3] " choice
+	case $choice in
+		1) one ;;
+		2) two ;;
+		3) three ;;
+		*) echo -e "${RED}Error...${STD}" && sleep 2
+	esac
+}
+ 
+# ----------------------------------------------
+# Step #3: Trap CTRL+C, CTRL+Z and quit singles
+# ----------------------------------------------
+trap '' SIGINT SIGQUIT SIGTSTP
+ 
+# -----------------------------------
+# Step #4: Main logic - infinite loop
+# ------------------------------------
+while true
+do
+ 	show_menus
+	read_options
+done
+
+
+
+
+
+
+
+
+#read Filename
+#while : 
+#do
+#printf "Wie lange soll gemessen werden in Sekunden: "
+#read Sekunden 
+#[ $Sekunden -ge 0 ] 1>/dev/null 2>&1 && break
+#done
+#script -a ./Ergebnis/$Filename.txt -c "./sem-6000.exp Volt --measure header --measure $Sekunden"
+
